8749ea010172d9560fde199286fca8aa0d9c7fdc
Dickon Reed <dickon@cantab.net>
2017-01-28 14:59:29 +0000

    get config in client

--- client.ts
+++ client.ts
@@ -1,3 +1,10 @@
-import * as $ from "jquery";
+/// <reference path="jquery.d.ts" />
 
-console.log("done");
+$.getJSON('/api/configs', configs=> {
+    if (configs.length != 1) {
+        console.log("did not get exactly one config");
+    } else {
+        let config = configs[0].name;
+        console.log(`using config ${config}`);
+    }
+});
