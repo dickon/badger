cc121d72e46b3697a6035e3107c1a55dc721cfad
Dickon Reed <dickon@cantab.net>
2017-01-28 14:04:46 +0000

    add a simple database

--- app.js
+++ app.js
@@ -1,15 +1,8 @@
 "use strict";
 var express = require("express");
-var fs = require("fs");
 //import * as bodyParser from "body-parser"
 var app = express();
 app.use(express.static('public'));
-//app.use(bodyParser.json());
-//app.use(bodyParser.urlencoded({extended:true}));
-app.get("/", function (req, res) { return fs.readFile(__dirname + "/index.html", function (err, data) {
-    if (err)
-        return console.error("file read error " + err);
-    res.send(data);
-}); });
+app.get('/js/client.js', function (req, res) { return res.sendFile(__dirname + '/build/client.js'); });
 var server = app.listen(3000, function () { return console.log("listening on " + server.address().port); });
 //# sourceMappingURL=app.js.map
--- app.ts
+++ app.ts
@@ -1,13 +1,15 @@
 import * as express from "express";
 import * as fs from "fs";
+import * as sqlite3 from "sqlite3";
+declare var __dirname:string;
 //import * as bodyParser from "body-parser"
 let app = express();
+let db = new sqlite3.Database("test.sqlite3");
 app.use(express.static('public'));
-//app.use(bodyParser.json());
-//app.use(bodyParser.urlencoded({extended:true}));
-app.get("/", (req,res)=> fs.readFile(__dirname + "/index.html", (err, data) => {
-       if (err) return console.error(`file read error ${err}`);
-        res.send(data);
+app.get('/api/configs', (req, res) => 
+    db.all('select * from configs', (err, rows) => {
+        if (err != null) return console.log("query error "+err);
+        res.json(rows);
     }));
+app.get('/js/client.js', (req, res) => res.sendFile(__dirname+'/build/client.js'));
 let server = app.listen(3000, () => console.log(`listening on ${server.address().port}`)); 
-
