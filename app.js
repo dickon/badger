79b4e8a7f9b822f0552bf750aaa2aa82315b984e
Dickon Reed <dickon@cantab.net>
2017-01-28 14:26:48 +0000

    list image directory contetns

--- app.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var express = require("express");
-//import * as bodyParser from "body-parser"
-var app = express();
-app.use(express.static('public'));
-app.get('/js/client.js', function (req, res) { return res.sendFile(__dirname + '/build/client.js'); });
-var server = app.listen(3000, function () { return console.log("listening on " + server.address().port); });
-//# sourceMappingURL=app.js.map
--- app.ts
+++ app.ts
@@ -8,8 +8,23 @@ let db = new sqlite3.Database("test.sqlite3");
 app.use(express.static('public'));
 app.get('/api/configs', (req, res) => 
     db.all('select * from configs', (err, rows) => {
-        if (err != null) return console.log("query error "+err);
+        if (err != null) {
+            console.log("query error "+err);
+            return;
+        }
         res.json(rows);
     }));
+app.get('/api/config/:config/images', (req, res) => {
+    console.log("name "+req.params.config);
+    db.get('select image_directory from configs where name=?', req.params.config, (err, row) => {
+        console.log("row "+row);
+        if (err != null) res.status(500).send({error:'query '+err});
+        else fs.readdir(row.image_directory, (rErr, items) => {
+            if (rErr) res.status(500).send({error:'readdir ' +err});
+            else res.json(items); 
+        });
+    });
+});
 app.get('/js/client.js', (req, res) => res.sendFile(__dirname+'/build/client.js'));
+console.log("running");
 let server = app.listen(3000, () => console.log(`listening on ${server.address().port}`)); 
