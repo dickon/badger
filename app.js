9bfbd469a4c99d6e657f377ffc5cb185db29a4b5
Dickon Reed <dickon@cantab.net>
2017-01-28 11:31:47 +0000

    add static content for front page

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
-var Startup = (function () {
-    function Startup() {
-    }
-    Startup.main = function () {
-        console.log("hello my world");
-        return 0;
-        Errorerro;
-        Error;
-    };
-    return Startup;
-}());
-Startup.main();
+"use strict";
+var express = require("express");
+var fs = require("fs");
+//import * as bodyParser from "body-parser"
+var app = express();
+app.use(express.static('public'));
+//app.use(bodyParser.json());
+//app.use(bodyParser.urlencoded({extended:true}));
+app.get("/", function (req, res) { return fs.readFile(__dirname + "/index.html", function (err, data) {
+    if (err)
+        return console.error("file read error " + err);
+    res.send(data);
+}); });
+var server = app.listen(3000, function () { return console.log("listening on " + server.address().port); });
 //# sourceMappingURL=app.js.map
--- app.ts
+++ app.ts
@@ -1,9 +1,13 @@
 import * as express from "express";
+import * as fs from "fs";
 //import * as bodyParser from "body-parser"
 let app = express();
-
+app.use(express.static('public'));
 //app.use(bodyParser.json());
 //app.use(bodyParser.urlencoded({extended:true}));
-app.get("/", (req,res)=> res.send("hello world"));
+app.get("/", (req,res)=> fs.readFile(__dirname + "/index.html", (err, data) => {
+       if (err) return console.error(`file read error ${err}`);
+        res.send(data);
+    }));
 let server = app.listen(3000, () => console.log(`listening on ${server.address().port}`)); 
 
