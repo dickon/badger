432ed91c570fa63179fd6b6f769bda9a61e281d1
Dickon Reed <dickon@cantab.net>
2017-01-30 22:22:02 +0000

    remove repetition of get json

--- app.ts
+++ app.ts
@@ -7,19 +7,17 @@ let app = express();
 let sizeofPromise:(path:string)=>Promise<any> = Promise.denodeify(sizeOf);
 let readdir:(path:string)=>Promise<string[]> = Promise.denodeify(fs.readdir);
 let knex: client = client({client:'sqlite3', useNullAsDefault: true, connection: { filename: "test.sqlite3"}});
-let jsonResponse = (res: any, x: Promise) => x.then(x => res.json(x)).catch(err => res.status(500).send({error:err}));
 let getImageDirectory = (req): Promise<string> => knex.select('image_directory').from('configs').where('name', req.params.config).first().then(x=>x.image_directory);
 let getBackgroundImageFile = (req): Promise<string> => knex.select('background_image_file').from('configs').where('name', req.params.config).first().then(x=>x.background_image_file);
+let jsonGet = (urlPattern, fn) => app.get(urlPattern, (req,res)=> fn(req).then(x=>res.json(x)).catch(err => res.status(500).send({error:err})));
 
 //import * as bodyParser from "body-parser"
 
 app.use(express.static('public'));
-app.get('/api/configs', (req, res) => jsonResponse(res, knex.select('*').from('configs')));
-app.get('/api/configs/:config/badges', (req, res) => jsonResponse(res, knex('badges').join('configs', 'badges.configId', '=', 'configs.id')
-            .select('first', 'last', 'badges.id', 'badges.filename', 'badges.rotation').where('configs.name', req.params.config)));
-app.get('/api/configs/:config/images', (req, res) => 
-    getImageDirectory(req).then(i=>readdir(i)).then(items=>res.json(items.filter(x=>x.toLowerCase().endsWith('.jpg'))))
-    .catch(err=>res.status(500).send({error:'query '+err})));
+jsonGet('/api/configs', req => knex.select('*').from('configs'));
+jsonGet('/api/configs/:config/badges', req => knex('badges').join('configs', 'badges.configId', '=', 'configs.id')
+            .select('first', 'last', 'badges.id', 'badges.filename', 'badges.rotation').where('configs.name', req.params.config));
+jsonGet('/api/configs/:config/images', req => getImageDirectory(req).then(i=>readdir(i)).then(items=>items.filter(x=>x.toLowerCase().endsWith('.jpg'))));
 app.get('/api/configs/:config/image/:image', (req, res) => getImageDirectory(req).then(i=> {
     let match = req.params.image.match(/[0-9\.a-zA-Z\-_]/);
     if (match == null) res.status(500).send({error:'bad image name'}); 
