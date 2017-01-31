42d8df76f7d0327b09a4e0569e6fc7cc4e8294c3
Dickon Reed <dickon@cantab.net>
2017-01-31 23:09:27 +0000

    work toward scaling

--- client.ts
+++ client.ts
@@ -17,6 +17,8 @@ interface Badge {
     id: string;
     filename: string;
     rotation: number;
+    imageWidth: number;
+    imageHeight: number;
 }
 
 class Editor {
@@ -47,20 +49,30 @@ class Editor {
 
     render(badgeId) {
         let badge = this.badgemap[badgeId];
-        let svg = `<image width="500" height="300" visibility="visibile" href="/api/configs/${this.config}/background"></image>`;
+        let svg = `<image width="880" height="480" visibility="visibile" href="/api/configs/${this.config}/background"></image>`;
         if (badge.filename) {
-            svg += `<g transform="translate(350 5)"><image draggable="true"  ondragstart="imageDrag(event, '${badge.filename}')" transform='scale(0.8) rotate(${badge.rotation} 150 200) ' class="thumbnail" href="/api/configs/${this.config}/image/${badge.filename}"> </image></g>`;
+            let x= badge.x == null ? 0 : badge.x;
+            let y = badge.y == null ? 0 : badge.y;
+            let width = badge.width == null ? badge.imageWidth : badge.width;
+            let height = badge.height == null ? badge.imageHeight : badge.height; 
+            console.log(JSON.stringify(badge));
+            svg += `<defs> <clipPath id="iclip"> <rect x="${x}" y="${y}" width="${width}" height="${height}"></rect></clipPath></defs>`;
+            svg += `<g transform="translate(380 50)"><image draggable="true" ondragstart="imageDrag(event, '${badge.filename}')" clip-path="url('#iclip')" transform='scale(1.5) rotate(${badge.rotation} 180 100) ' class="thumbnail" href="/api/configs/${this.config}/image/${badge.filename}"> </image></g>`;
         }
-        svg += `<text x=100 y=100 style="font-size: 20pt; text-anchor: middle">${capitalise(badge.first)}</text>`;
-        svg += `<text x=100 y=200 style="font-size: 40pt; text-anchor: middle">${capitalise(badge.last)}</text>`;
+        svg += `<text x=120 y=280 style="font-size: 20pt; text-anchor: middle">${capitalise(badge.first)}</text>`;
+        svg += `<text x=120 y=380 style="font-size: 40pt; text-anchor: middle">${capitalise(badge.last)}</text>`;
         $(`#badge${badgeId}`).html(`<svg class="badge" ondragover="allowDrop(event)" ondrop="drop(event, '${this.config}', ${badge.id}, '${badge.first}', '${badge.last}')">${badge.first} ${badge.last} ${svg}`);
     }
 
     createBadge(badge) {
         this.badgemap[badge.id] = badge;
-        console.log(`create badge ${JSON.stringify(badge)}`);
-        $('#badges').append(`<svg class="badge" id="badge${badge.id}"></svg>`);
-        this.render(badge.id);
+        $.getJSON(`/api/configs/${this.config}/image/${badge.filename}/size`, imageSize => {
+            badge.imageWidth = imageSize.width;
+            badge.imageHeight = imageSize.height;
+            console.log(`image size ${imageSize.width} ${imageSize.height}`);
+            $('#badges').append(`<svg class="badge" id="badge${badge.id}"></svg>`);
+            this.render(badge.id);
+        });
     }
 
     loadBadges() {
