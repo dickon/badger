818ccbd007232c0bd187f8a42b8f472f146aeb62
Dickon Reed <dickon.reed@citrix.com>
2017-02-08 23:17:18 +0000

    fix image update

--- client.ts
+++ client.ts
@@ -34,6 +34,10 @@ interface Config {
     name: string;
     badgeWidth: number;
     badgeHeight: number;
+    imageLeft: number;
+    imageRight: number;
+    imageTop: number;
+    imageBottom: number;
 }
 
 interface HTMLElement {
@@ -71,9 +75,9 @@ class Editor {
             type:'PUT',
             success: (result) => {
                 console.log(`put to server ${index}`);
-                this.badges[index].filename = data;
-                this.render(index);
-                console.log(`redrew ${index}`);
+                let badge = this.badges[index];
+                badge.filename = data;
+                this.sizeBadge(badge);
             }
         });
     }
@@ -103,22 +107,34 @@ class Editor {
         let badge = this.badges[id];
         console.log(`create ${badgeKey}`);
         this.badgemap[badge.id] = badge;
+        if (badge.filename == null) {
+            this.processBadge(badge);
+        } else {
+            this.sizeBadge(badge);
+        }
+    }
+
+    sizeBadge(badge) {
         $.getJSON(`/api/configs/${this.config.name}/image/${badge.filename}/size${this.lowPostfix}`, imageSize => {
             badge.imageWidth = imageSize.width;
             badge.imageHeight = imageSize.height;
-            $('#badges').append(`<span class="badgeContainer" id="badge${badge.id}" onclick="editor.select(${badge.id})"><svg class="badge" id="badgeSvg${badge.id}" width="${this.config.badgeWidth}mm" height="${this.config.badgeHeight}mm" viewbox="0 0 ${this.config.badgeWidth} ${this.config.badgeHeight}" ondragover="allowDrop(event)" ondrop="editor.drop(event, ${badge.id})"> </svg></span>`);
-            let paper = Snap(`#badgeSvg${badge.id}`);
-            paper.image(`/api/configs/${this.config.name}/background`, 0,0, this.config.badgeWidth, this.config.badgeHeight);
-            for (var name of ['first', 'last', 'title']) {
-                let text = paper.text(this.config.badgeWidth*0.245, this.config.badgeHeight*(name=='first' ? 0.55 : (name == 'title'? 0.91 : 0.75 )), 
-                    capitalise(badge[name])).attr({'font-family': 'Arial', 'text-anchor':'middle', fill:(name == 'title' ? '#c0c40b':'white'), stroke:'none', 'font-size':'10pt' });
-                let bbox = text.getBBox();
-                text.attr({'style.font-size': `${Math.min(this.config.badgeHeight*(name == 'first' ? 0.35:0.2)*10/bbox.height, 
-                                                         this.config.badgeWidth*0.35*10/bbox.width)}pt`});
-                text.attr({filter: paper.filter(Snap.filter.shadow(0.5, 0.5, 0.2, "black", 0.7))});
-            }           
-            this.render(badge.id);
+            this.processBadge(badge);
         });
+    }
+
+    processBadge(badge) {
+        $('#badges').append(`<span class="badgeContainer" id="badge${badge.id}" onclick="editor.select(${badge.id})"><svg class="badge" id="badgeSvg${badge.id}" width="${this.config.badgeWidth}mm" height="${this.config.badgeHeight}mm" viewbox="0 0 ${this.config.badgeWidth} ${this.config.badgeHeight}" ondragover="allowDrop(event)" ondrop="editor.drop(event, ${badge.id})"> </svg></span>`);
+        let paper = Snap(`#badgeSvg${badge.id}`);
+        paper.image(`/api/configs/${this.config.name}/background`, 0,0, this.config.badgeWidth, this.config.badgeHeight);
+        this.render(badge.id);
+        for (var name of ['first', 'last', 'title']) {
+            let text = paper.text(this.config.badgeWidth*0.245, this.config.badgeHeight*(name=='first' ? 0.55 : (name == 'title'? 0.91 : 0.75 )), 
+                capitalise(badge[name])).attr({'font-family': 'Arial', 'text-anchor':'middle', fill:(name == 'title' ? '#c0c40b':'white'), stroke:'none', 'font-size':'10pt' });
+            let bbox = text.getBBox();
+            text.attr({'style.font-size': `${Math.min(this.config.badgeHeight*(name == 'first' ? 0.35:0.2)*10/bbox.height, 
+                                                        this.config.badgeWidth*0.35*10/bbox.width)}pt`});
+            text.attr({filter: paper.filter(Snap.filter.shadow(0.5, 0.5, 0.2, "black", 0.7))});
+        }           
     }
 
     render(badgeId) {
@@ -126,56 +142,58 @@ class Editor {
         let oldImage=Snap(`#badgeImage${badgeId}`);
         if (oldImage != null) oldImage.remove();
         let paper = Snap(`#badgeSvg${badgeId}`);
-        const imLeft = 0.45;
-        const imRight = 0.98;
-        const imTop = 0.025;
-        const imBottom = 0.975;
-        const imXCentre = this.config.badgeWidth * (imLeft + imRight)/2;
-        const imYCentre = this.config.badgeHeight * (imTop + imBottom)/2
-        const portWidth = (imRight - imLeft) * this.config.badgeWidth;
-        const portHeight = (imBottom - imTop) * this.config.badgeHeight;
-        const portRatio = portHeight / portWidth;
-        const imHeight = imBottom - imTop
+        if (badge.filename != null) {
+            const imLeft = this.config.imageLeft;
+            const imRight = this.config.imageRight;;
+            const imTop = this.config.imageTop;
+            const imBottom = this.config.imageBottom;
+            const imXCentre = this.config.badgeWidth * (imLeft + imRight)/2;
+            const imYCentre = this.config.badgeHeight * (imTop + imBottom)/2
+            const portWidth = (imRight - imLeft) * this.config.badgeWidth;
+            const portHeight = (imBottom - imTop) * this.config.badgeHeight;
+            const portRatio = portHeight / portWidth;
+            const imHeight = imBottom - imTop
 
-        if (badge.left == null) badge.left = 0;
-        if (badge.right == null) badge.right = 0;
-        if (badge.top == null) badge.top = 0;
-        if (badge.bottom == null) badge.bottom = 0;
-        const originalAspectRatio = badge.imageHeight / badge.imageWidth;
-        const rotatedImageWidth = badge.rotation == 0 ? badge.imageWidth : badge.imageHeight;
-        const rotatedImageHeight = badge.rotation == 0 ? badge.imageHeight : badge.imageWidth;
-        const rotatedAspectRatio = rotatedImageHeight / rotatedImageWidth;
-        const clipBoxRatio = (1-badge.top-badge.bottom) / (1-badge.right-badge.left);
-        const clippedRatio = rotatedAspectRatio * clipBoxRatio;
+            if (badge.left == null) badge.left = 0;
+            if (badge.right == null) badge.right = 0;
+            if (badge.top == null) badge.top = 0;
+            if (badge.bottom == null) badge.bottom = 0;
+            const originalAspectRatio = badge.imageHeight / badge.imageWidth;
+            const rotatedImageWidth = badge.rotation == 0 ? badge.imageWidth : badge.imageHeight;
+            const rotatedImageHeight = badge.rotation == 0 ? badge.imageHeight : badge.imageWidth;
+            const rotatedAspectRatio = rotatedImageHeight / rotatedImageWidth;
+            const clipBoxRatio = (1-badge.top-badge.bottom) / (1-badge.right-badge.left);
+            const clippedRatio = rotatedAspectRatio * clipBoxRatio;
 
-        const hfit = clippedRatio < portRatio;
+            const hfit = clippedRatio < portRatio;
 
-        const visibleWidth = hfit ?  portWidth                : portHeight / clippedRatio;
-        const visibleHeight = hfit ?  portWidth*clippedRatio  : portHeight;
+            const visibleWidth = hfit ?  portWidth                : portHeight / clippedRatio;
+            const visibleHeight = hfit ?  portWidth*clippedRatio  : portHeight;
 
-        const clippedFullWidth = ( badge.rotation == 0 ? visibleWidth * (1 + badge.left + badge.right) : visibleHeight * (1 + badge.top + badge.bottom));
-        const clippedFullHeight = originalAspectRatio*clippedFullWidth;
+            const clippedFullWidth = ( badge.rotation == 0 ? visibleWidth * (1 + badge.left + badge.right) : visibleHeight * (1 + badge.top + badge.bottom));
+            const clippedFullHeight = originalAspectRatio*clippedFullWidth;
 
-        const scale = (badge.rotation == 0) ? (clippedFullHeight < visibleHeight ? visibleHeight/clippedFullHeight : 1):
-                                             (clippedFullWidth < visibleWidth ? visibleWidth / clippedFullHeight : 1);
-        const fullWidth = scale * clippedFullWidth;
-        const fullHeight = (fullWidth * originalAspectRatio);
+            const scale = (badge.rotation == 0) ? (clippedFullHeight < visibleHeight ? visibleHeight/clippedFullHeight : 1):
+                                                (clippedFullWidth < visibleWidth ? visibleWidth / clippedFullHeight : 1);
+            const fullWidth = scale * clippedFullWidth;
+            const fullHeight = (fullWidth * originalAspectRatio);
 
-        const imagePositionLeft = imXCentre-fullWidth*(badge.left - badge.right + 1)/2;  
-        const imagePositionTop = imYCentre-fullHeight*(badge.top - badge.bottom +1)/2;
-        //paper.rect(imLeft*this.config.badgeWidth, imTop*this.config.badgeHeight, (imRight-imLeft)*this.config.badgeWidth, (imBottom-imTop)*this.config.badgeHeight).attr({fill:'red'});
-        let im = paper.image(`/api/configs/${this.config.name}/image/${badge.filename}${this.lowPostfix}`, 
-                 imagePositionLeft, imagePositionTop, fullWidth, fullHeight);
-        if (badge.brightness == null) badge.brightness = 1.0;
-        if (badge.brightness != 1)
-             im.attr({filter: paper.filter(Snap.filter.brightness(badge.brightness/2))});
-        im.transform(`r${badge.rotation}`);
-        let cliprect = paper.rect(imXCentre - visibleWidth/2, imYCentre - visibleHeight/2, 
-                                visibleWidth, visibleHeight).attr({fill:'#fff'});
-        let group = paper.group(im);
-        group.attr({mask:cliprect});
-        let g2 = paper.group(group).attr({id:`badgeImage${badgeId}`});
-        g2.attr({filter: paper.filter(Snap.filter.shadow(0.5, 0.5, 0.2, "black", 0.9))});
+            const imagePositionLeft = imXCentre-fullWidth*(badge.left - badge.right + 1)/2;  
+            const imagePositionTop = imYCentre-fullHeight*(badge.top - badge.bottom +1)/2;
+            //paper.rect(imLeft*this.config.badgeWidth, imTop*this.config.badgeHeight, (imRight-imLeft)*this.config.badgeWidth, (imBottom-imTop)*this.config.badgeHeight).attr({fill:'red'});
+            let im = paper.image(`/api/configs/${this.config.name}/image/${badge.filename}${this.lowPostfix}`, 
+                    imagePositionLeft, imagePositionTop, fullWidth, fullHeight);
+            if (badge.brightness == null) badge.brightness = 1.0;
+            if (badge.brightness != 1)
+                im.attr({filter: paper.filter(Snap.filter.brightness(badge.brightness/2))});
+            im.transform(`r${badge.rotation}`);
+            let cliprect = paper.rect(imXCentre - visibleWidth/2, imYCentre - visibleHeight/2, 
+                                    visibleWidth, visibleHeight).attr({fill:'#fff'});
+            let group = paper.group(im);
+            group.attr({mask:cliprect});
+            let g2 = paper.group(group).attr({id:`badgeImage${badgeId}`});
+            g2.attr({filter: paper.filter(Snap.filter.shadow(0.5, 0.5, 0.2, "black", 0.9))});
+        }
         // paper.circle(imXCentre, imYCentre, 2).attr({fill:'red'});
         // paper.text(3,3, `${hfit?'hfit':'vfit'} ${badge.rotation==0?"straight":"rotated"} visible ${Math.floor(visibleWidth)}x${Math.floor(visibleHeight)} port ${Math.floor(portWidth)}x${Math.floor(portHeight)} full ${Math.floor(fullWidth)}x${Math.floor(fullHeight)} (scale ${scale})`).attr({'font-size':'2pt', fill:'white'});
         // paper.text(3,6, `original aspect ratio ${originalAspectRatio.toPrecision(3)} rotated aspect ratio ${rotatedAspectRatio.toPrecision(3)} clipBoxRatio ${clipBoxRatio.toPrecision(3)} clipped ratio ${clippedRatio.toPrecision(3)} port ratio ${portRatio.toPrecision(3)} full ratio ${(fullHeight/fullWidth).toPrecision(3)} visible ratio ${(visibleHeight/visibleWidth).toPrecision(3)}`).attr({'font-size':'1.1pt', fill:'white'});
